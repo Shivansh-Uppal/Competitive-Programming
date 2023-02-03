5a26c96d3145aaa9cde336c1bf6f9a362bf5b227
Shivansh uppal <74812139+Shivansh-Uppal@users.noreply.github.com>
2023-02-03 21:29:28 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            if(j == 0 || j == i){
+                cout << 1 << " ";
+            }
+            else{
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    }
+
+return 0;
+}
