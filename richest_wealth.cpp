7ba2f74af5c77c2f2f2b97fd49d478dabb7c5f15
Shivansh uppal <74812139+Shivansh-Uppal@users.noreply.github.com>
2023-02-04 22:19:29 +0530

    Add files via upload

---
+++ richest_wealth.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max=0;
+        int sum=0;
+        for(int i=0;i<accounts.size();i++){
+            for(int j=0;j<accounts[i].size();j++){
+                sum=sum+accounts[i][j];
+            }
+            if(sum>max){
+                max=sum;
+            }
+            sum=0;
+        }
+        return max;
+        
+    }
+};
