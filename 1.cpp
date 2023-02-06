1a12b2c2a7de0b0d7aab78f90a914ae279f49cf0
Shivansh uppal <74812139+Shivansh-Uppal@users.noreply.github.com>
2023-02-06 23:22:15 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
+
+//wap for function overloading for calculating volume of shapes
 using namespace std;
+
+int volume(int r){
+    return (4/3)*3.14*r*r*r;
+}
+
+int volume(int l,int b,int h){
+    return l*b*h;
+}
+
 int main(){
-    int n;
-
-    cin >> n;
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j <= i; j++){
-            if(j == 0 || j == i){
-                cout << 1 << " ";
-            }
-            else{
-                cout << i << " ";
-            }
-        }
-        cout << endl;
-    }
-
-return 0;
+    int r,l,b,h,a;
+    cout<<"Enter radius of sphere: ";
+    cin>>r;
+    cout<<"Volume of sphere is: "<<volume(r)<<endl;
+    cout<<"Enter length, breadth and height of cuboid: ";
+    cin>>l>>b>>h;
+    cout<<"Volume of cuboid is: "<<volume(l,b,h)<<endl;
+    return 0;
 }
